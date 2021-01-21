e7eabcd5d494ce5349f0a3196dd83b0382a7330e
Amine <aminefilali@outlook.dk>
2021-01-21 19:03:34 +0100

    redirect to home if signed in + userActionTypes added

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './app.scss';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
@@ -43,15 +43,22 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
-          <Route exact path="/login" component={LoginPage} />
+          <Route
+            exact
+            path="/login"
+            render={() => this.props.currentUser ? (<Redirect to='/' />) : (<LoginPage />)} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
