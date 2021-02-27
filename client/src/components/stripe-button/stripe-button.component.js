7df223ada2a9e59d333200ee68f624addb8feeb2
Amine <aminefilali@outlook.dk>
2021-02-27 10:20:31 +0100

    server created + stripe payment finalized

--- client/src/components/stripe-button/stripe-button.component.js
+++ client/src/components/stripe-button/stripe-button.component.js
@@ -1,8 +1,26 @@
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const stripePrice = price * 100;
   const publishableKey = 'pk_test_51IIzrRKoV5E4VrlkIdIzlH0OJcwPI2SxvqH7WSpwEVQ8r9DXgpxzCaiHAEIPsFTFWdNYG9ei3n94YDmbErLMHf2j002qlCcuZx';
+
+  const onToken = (token) => {
+    const axiosConfig = {
+      url: 'payment',
+      method: 'post',
+      data: {
+        token,
+        amount: stripePrice
+      }
+    }
+    axios(axiosConfig).then(res => {
+      alert('Payment successful !');
+    }).catch(error => {
+      console.error('Payment error: ', JSON.parse(error));
+      alert('There was an issue with your payment, make sure to use the test credit card.');
+    });
+  }
 
   return (
     <StripeCheckout
@@ -14,15 +32,10 @@ const StripeCheckoutButton = ({ price }) => {
       description={`Your total is $${price}`}
       amount={stripePrice}
       panelLabel="Pay now"
-      token={onPaymentSuccess}
+      token={onToken}
       stripeKey={publishableKey}
     />
-  )
-}
-
-const onPaymentSuccess = token => {
-  console.log(token);
-  alert('Payment successful');
+  );
 }
 
 export default StripeCheckoutButton;
